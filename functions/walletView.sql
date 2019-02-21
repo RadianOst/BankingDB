9ce97dc87209fb0fe134eedc19dd6eba2e735a7b
Krzysztof Odoj <kodoj1@o2.pl>
2019-02-21 19:50:12 +0100

    Wallet View

--- functions/walletView.sql
+++ functions/walletView.sql
@@ -1,2 +1,11 @@
-CREATE VIEW WALLET AS
-  SELECT balance, number FROM accounts
+CREATE VIEW wallet AS
+SELECT accounts.balance      AS account_balance,
+       accounts.number       AS account_number,
+       credit_types.name     AS credit,
+       credits.value         AS credit_value,
+       investment_types.name AS investment,
+       investments.start_date
+FROM (((accounts LEFT JOIN credits ON accounts.user_id = credits.user_id )
+  LEFT JOIN credit_types ON credit_types.credit_type_id = credits.credit_type_id)
+  LEFT JOIN investments ON credits.user_id = investments.user_id)
+       LEFT JOIN investment_types ON investments.investment_type_id = investment_types.investment_type_id;
