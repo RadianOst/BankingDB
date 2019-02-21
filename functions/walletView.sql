cf3615c9a4a926f8b3419c2eb500e27aaa805d75
Krzysztof Odoj <kodoj1@o2.pl>
2019-02-21 20:00:18 +0100

    wallet reworked to show user name

--- functions/walletView.sql
+++ functions/walletView.sql
@@ -1,11 +1,18 @@
 CREATE VIEW wallet AS
-SELECT accounts.balance      AS account_balance,
+SELECT users.name            AS name,
+       accounts.balance      AS account_balance,
        accounts.number       AS account_number,
        credit_types.name     AS credit,
        credits.value         AS credit_value,
        investment_types.name AS investment,
        investments.start_date
-FROM (((accounts LEFT JOIN credits ON accounts.user_id = credits.user_id )
+FROM ((((users LEFT JOIN accounts on users.user_id = accounts.user_id)
+  LEFT JOIN credits ON accounts.user_id = credits.user_id )
   LEFT JOIN credit_types ON credit_types.credit_type_id = credits.credit_type_id)
   LEFT JOIN investments ON credits.user_id = investments.user_id)
-       LEFT JOIN investment_types ON investments.investment_type_id = investment_types.investment_type_id;
+  LEFT JOIN investment_types ON investments.investment_type_id = investment_types.investment_type_id;
+
+
+drop VIEW wallet;
+
+select * FROM wallet;
